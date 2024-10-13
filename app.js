6595bd6251510123b4ac61be3910dbfc9507f3a1
le.dinh.tam.phung <le.dinh.tam.phung@tanaakk.com>
2024-10-13 21:42:48 +0700

    commit

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express')
-//const configViewEngine = require('./src/config/viewEngine');
+const cors = require('cors');
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 8082
+
+
+// Cấu hình CORS
+app.use(cors({
+  origin: 'http://localhost:3000', // Địa chỉ frontend
+  credentials: true // Cho phép cookie nếu cần
+}));
 
 const webRouter = require('./src/router/web');
 const userRoutes = require('./src/router/userRouter');
