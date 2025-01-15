0c09f1a9b4b461622dabe263d674f59c02fa0b04
ldtamphung <44759329+ldtamphung@users.noreply.github.com>
2025-01-15 11:39:12 +0700

    Update homeController.js

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -2,7 +2,7 @@
 
 const getHomePage = (req ,res) =>{
     //xu li data call model
-    res.send('Hello Tam Phung den voi NodeJs deploy test auto');
+    res.send('Hello Tam Phung den voi NodeJs deploy test auto 1');
 }
 module.exports = {
     getHomePage
