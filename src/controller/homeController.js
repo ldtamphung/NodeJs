d56da8a7012c927df2b7d100e101134c0d5360e0
ldtamphung <phungle.291197@gmail.com>
2025-01-16 21:56:20 +0700

    add commit new

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -2,7 +2,7 @@
 
 const getHomePage = (req ,res) =>{
     //xu li data call model
-    res.send('Hello Tam Phung den voi NodeJs deploy test auto 1001');
+    res.send('CICD Node.js webhook sử dụng PM2 và GitHub Webhook:');
 }
 module.exports = {
     getHomePage
